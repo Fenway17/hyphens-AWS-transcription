f49402ca467105ab322f0af3f4bd70d1a7cc2fc6
Tan Yong Rui <tanyongruifenway@gmail.com>
2024-08-06 16:01:43 +0800

    Updated output to use folder structures. Updated readme file.

--- README.md
+++ README.md
@@ -6,16 +6,19 @@ Transcription of CS calls using AWS Transcribe
 
 Parameters:
 
--   Edit script to include correct parameters to the variables needed
+-   Edit `transcribe_folder.sh` script to include correct variables/parameters to the variables needed
 
 Run the Script:
 
--   Open Git Bash
+-   Open Git Bash (Requires Git to be installed)
 -   Navigate to the directory containing your transcribe_folder.sh script
+    -   Alternatively: Navigate to directory, and right click and click `Open Git Bash Here`
 -   Make the script executable (if necessary) with `chmod +x transcribe_folder.sh`
 -   Run the script with `./transcribe_folder.sh`
 
-Process the transcription json files:
+Process the transcription json files into human readable text:
 
--   use `python src/process_transcriptions.py transcriptions processed-transcriptions`
--   alternatively, `npm run process-transcriptions`
+-   Transfer JSON files to `transcriptions` folder
+-   Use `python src/process_transcriptions.py transcriptions processed-transcriptions`
+    -   Alternatively, `npm run process-transcriptions`
+-   Results will be output into `processed-transcriptions` folder
--- transcribe_folder.sh
+++ transcribe_folder.sh
@@ -2,10 +2,11 @@
 
 # Configuration
 BUCKET_NAME="cs-calls-audio"
+AUDIO_FOLDER="rawcalls-feb2024/2024-08-06T03_30_10.877Z"
 OUTPUT_BUCKET="cs-calls-transcription"
+OUTPUT_FOLDER="transcription-feb-2024"
 LANGUAGE_OPTIONS=("en-US" "zh-CN" "ms-MY")
 JOB_NAME_PREFIX="call-transcription-job-"
-AUDIO_FOLDER="rawcalls-jan2024"
 # Currently unable to use this custom vocab as it is tied to english only
 CUSTOM_VOCABULARY_NAME="PharmaceuticalTerms"
 SPEAKER_SEPARATION=true
@@ -27,6 +28,7 @@ function output_file_exists {
     return 1 # Unsupported file extension
   fi
 
+  output_file_path="$OUTPUT_FOLDER/$output_file_path"
   local existing_output=$(aws s3 ls s3://$OUTPUT_BUCKET/$output_file_path)
   echo "Searching for file at: $output_file_path"
   
@@ -52,6 +54,7 @@ for FILE in $FILES; do
         --transcription-job-name "$JOB_NAME" \
         --media MediaFileUri="$MEDIA_FILE_URI" \
         --output-bucket-name "$OUTPUT_BUCKET" \
+        --output-key "$OUTPUT_FOLDER/$JOB_NAME.json" \
         --identify-language \
         --language-options "${LANGUAGE_OPTIONS[@]}" \
         --settings "ShowSpeakerLabels=$SPEAKER_SEPARATION,MaxSpeakerLabels=$MAX_SPEAKERS"
